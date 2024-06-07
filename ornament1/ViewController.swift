6e2e7c65054a06b0419b8ac17652b090a7b4564f
Васин Валерий Михайлович <vasinvm@akbarsdigital.ru>
2024-06-07 17:21:58 +0300

    add-project

---
+++ ornament1/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ornament1
+//
+//  Created by Eduard Sergeev on 02.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: USE Example
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
